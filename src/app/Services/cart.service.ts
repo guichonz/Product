d894b0bf133f953e2b88ac2a2fddd7e8f60f6f59
damienguiche <damien.guiche@gmail.com>
2019-12-19 14:13:58 +0100

    ajout des méthodes

--- src/app/Services/cart.service.ts
+++ src/app/Services/cart.service.ts
@@ -16,12 +16,31 @@ export class CartService {
 
   // Ajoute / Incrémente un produit au panier
   addProduct(product: Product) {
+    // Le produit en cours est il déja dans le panier ?
+    // const toto = this.productInfo.find(pInfo => pInfo.product.id === product.id);
+    const index = this.productInfo.findIndex(pInfo => pInfo.product.id === product.id);
+    if (index !== -1) {
+      // trouvé
+      this.productInfo[index].quantity ++; // produit + quantité
+      // this.productInfo[index] = {...pInfo, quantity: pInfo.quantity + 1};
+
+    } else {
+      // pas trouvé
+      this.productInfo.push({product: product, quantity: 1});
+    }
 
   }
 
   // Décrémente / Retire un produit du panier
   removeProduct(product: Product) {
-
+    const index = this.productInfo.findIndex(pInfo => pInfo.product.id === product.id);
+    if (index !== -1) {
+      // trouvé
+      this.productInfo[index].quantity --; // produit + quantité
+      if (this.productInfo[index].quantity === 0) {
+        this.productInfo.splice(index, 1);
+      }
+    }
   }
 
   // Renvoie le contenu du panier
