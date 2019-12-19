4d9c8186a68975a79d01bbea761955ba54fa8870
damienguiche <damien.guiche@gmail.com>
2019-12-19 10:56:49 +0100

    Filtres type de plat

--- src/app/Models/product.ts
+++ src/app/Models/product.ts
@@ -14,22 +14,43 @@ export class Product {
     stock: number;
     photo: string;
     price: number;
+    tags: any[];
+    nutritionalInfo: any[];
+    slug: string;
 
     constructor(options: {
         id: number;
         name: string;
-        type: ProductType;
+        type: string;
         description: string;
         stock: number;
         photo: string;
         price: number;
+        tags: any[];
+        nutritionalInfo: any[];
+        slug: string;
     }) {
         this.id = options.id;
         this.name = options.name;
-        this.type = options.type;
+        switch (options.type) {
+            case 'STARTER':
+                this.type = ProductType.Entree;
+                break;
+            case 'MAIN_COURSE':
+                this.type = ProductType.Plat;
+                break;
+            case 'DESSERT':
+                this.type = ProductType.Dessert;
+                break;
+            default:
+                this.type = ProductType.Plat;
+        }
         this.description = options.description;
         this.stock = options.stock;
         this.photo = options.photo;
         this.price = options.price;
+        this.tags = options.tags;
+        this.nutritionalInfo = options.nutritionalInfo;
+        this.slug = options.slug;
     }
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../Services/product.service';
-import { Product } from '../Models/product';
+import { Product, ProductType } from '../Models/product';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styles: []
 })
+
 export class ProductListComponent implements OnInit {
   products: Product[];
+  starters: Product[];
+  mainCourses: Product[];
+  desserts: Product[];
+
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
     this.productService.getProducts()
-      .subscribe(data => this.products = data);
+    .subscribe(data => {
+      this.products = data;
+      this.starters =  this.products.filter(p => p.type === ProductType.Entree);
+      this.mainCourses =  this.products.filter(p => p.type === ProductType.Entree);
+      this.desserts =  this.products.filter(p => p.type === ProductType.Dessert);
+    });
+
   }
 
 }
